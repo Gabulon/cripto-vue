1428c9c379497663eafe1ff86450882b5ea6bdfe
Gabulon <gabo.fajardo7@gmail.com>
2024-04-19 19:59:15 -0600

    ¿Que son los composables en Vue.js?

---
+++ src/composables/useCripto.js
@@ -0,0 +1,11 @@
+export default function useCripto() {
+    const cotizarMoneda = () =>{
+        console.log('Ctoasd')
+    }
+    const auth=true
+
+    return {
+        cotizarMoneda,
+        auth
+    }
+}
